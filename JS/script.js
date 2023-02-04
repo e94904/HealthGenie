bd6e826fc2a1d3a4e82072128689a8f9ffec5009
Aaron Dangc <adraging@gmail.com>
2023-02-04 01:33:13 -0800

    Initial commit
    File structure as well as firebase and jquery

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+console.log("HELLO WORLD");
+
+$("#hello").html('hi');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBVhQGGc-QattqPOa4jL6yfE8NxsKUp55I",
+  authDomain: "healthgenie-be064.firebaseapp.com",
+  projectId: "healthgenie-be064",
+  storageBucket: "healthgenie-be064.appspot.com",
+  messagingSenderId: "982085319771",
+  appId: "1:982085319771:web:67b3697b2de2393292937e",
+  measurementId: "G-NGFMTP42NK"
+};
+firebase.initializeApp(firebaseConfig);
+console.log('firebase has been implemented successfully');
+// TO DO
+// implement chatGPT
+// USE FIREBASE AND JQUERY FOR APP
