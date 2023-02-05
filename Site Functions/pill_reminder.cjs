59ce07b8945f4a60bf2db4d728b10fd0259fb208
e94904 <ericlam94904@gmail.com>
2023-02-04 17:15:19 -0800

    Code for pill reminder & health record
    Pill reminder that asks for the time in 24 hour format and notifies them when it's time to take their pills. Also features health record that asks the user for their information

---
+++ Site Functions/pill_reminder.cjs
@@ -0,0 +1,46 @@
+const prompt = require('prompt-sync')();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//console.log("This is a test")
+
+
+  var now = new Date();
+  now.setHours(now.getHours() - 8)
+  now.setDate(now.getDate())
+  console.log(now)
+
+  console.log("What time today do you need to take your medicaitons?");
+
+  const hour = prompt("Enter Hour (0-24). 24 HOUR TIME ");
+  const minutes = prompt("Enter Minute (0-59) ");
+
+  var pill_time = new Date();
+  pill_time.setDate(pill_time.getDate())
+  //console.log(pill_time);
+  pill_time.setMinutes(minutes)
+  //console.log(pill_time)
+  pill_time.setHours(hour - 8)
+  //console.log(pill_time)
+
+  console.log(pill_time)
+  console.log(now)
+
+  time_diff = pill_time - now
+  console.log(time_diff)
+
+  function timesUp(){
+    console.log("Take your pills now")
+  }
+
+
+  if (time_diff > 0){
+
+    setTimeout(timesUp, time_diff)
+
+  }
+  else{
+    timesUp
+  }
